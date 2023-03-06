106f7296f3392f6613cc49fccde6de2f13b6fed7
Anita Laha <freekitten@yandex.ru>
2023-03-06 22:47:52 +0300

    handle photo content

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
 
 token: str ='5665289787:AAHVqrTkOvkxMpJuylPtjwy7AXAGEq96r3g'
 bot: Bot = Bot(token = token)
 dp: Dispatcher = Dispatcher()
 
-@dp.message(Command(commands=["start"]))
 async def handle_start_command(message: Message):
     answer: str = 'Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь'
     #await message.answer(answer)
     await bot.send_message(message.chat.id, answer)
 
-@dp.message(Command(commands=['help']))
 async def handle_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
-@dp.message()
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
+
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
+dp.message.register(handle_start_command, Command(commands=["start"]))
+dp.message.register(handle_help_command,  Command(commands=['help']))
+dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
