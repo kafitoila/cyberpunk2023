81db0ee87c4c0f306059611d5d5dfa7fbc8e3331
Anita Laha <freekitten@yandex.ru>
2023-04-08 14:23:53 +0300

    implant uid added

--- demo/generate_uid.py
+++ demo/generate_uid.py
@@ -29,7 +29,7 @@ def random_choice_num():
 def generate_uid(function, uid_in_use) -> list:
     out = []
     count = 0
-    for _ in range(500):
+    for _ in range(2000):
         new = function()
         if new not in out and new not in uid_in_use:
             out.append(new)
@@ -68,13 +68,31 @@ def add_uid_implant_general(from_file,to_file,page_count):
         reader = csv.reader(ff)
         with open(to_file,'w', encoding='UTF8', newline='') as tf:
             writer = csv.writer(tf)
-            for _ in range(page_count):
-                for row in reader:
-                    user_id = filter_first_char(uid_num, zero)
-                    secret_id = filter_first_char(uid, drugs)
-                    wrt = [row[0], user_id, secret_id]
+            data: list = []
+            data.extend(reader)
+            for page in range(page_count):
+                page_name = [f'Page {page + 1}']
+                writer.writerow(page_name)
+                print(len(data))
+                for row in data:
+                    id = filter_first_char(uid, drugs)
+                    wrt = [row[0], id]
                     # print(wrt)
                     writer.writerow(wrt)
+                print(len(data))
+                writer.writerow('')
+
+def transpose_data(from_file,to_file):
+    with open(from_file,'r', encoding='CP1251', newline='') as ff:
+        reader = csv.reader(ff)
+        with open(to_file,'w', encoding='UTF8', newline='') as tf:
+            writer = csv.writer(tf)
+            for row in reader:
+                for r in row:
+                    if r != row[0] and r != '':
+                        id = filter_first_char(uid, drugs)
+                        wrt = [row[0], r, id]
+                        writer.writerow(wrt)
 
 def correct_uid(from_file,to_file):
     with open(from_file,'r', encoding='UTF8', newline='') as ff:
@@ -101,7 +119,10 @@ def load_model(from_file):
 def get_uid(model,col) -> list:
     uid: list = []
     for line in model:
-        uid.append(line[col])
+        try:
+            uid.append(line[col])
+        except IndexError:
+            pass
     return uid
 
 def get_collisions(uid_in_use) -> int:
@@ -113,13 +134,19 @@ def get_collisions(uid_in_use) -> int:
     return collision_count
 
 
-# add_uid_name('user.csv','user_w_code.csv')
-# correct_uid('user_full.csv','user_full_new.csv')
-# collision_count = get_collisions(uid_in_use)
-# print(f'Collisions found: {collision_count}')
+name_model = load_model('user_full.csv')
+uid_in_use = get_uid(name_model,2)
+implant_model = load_model('implant_print_general.csv')
+uid_in_use.extend(get_uid(implant_model,1))
+print(len(uid_in_use))
 
-# name_model = load_model('user_full.csv')
-# uid_in_use = get_uid(name_model,2)
+uid = generate_uid(random_choice, uid_in_use)
+uid_num = generate_uid(random_choice_num, uid_in_use)
 
-# uid = generate_uid(random_choice, uid_in_use)
-# uid_num = generate_uid(random_choice_num, uid_in_use)
+# add_uid_implant_general('implant_print_set.csv', 'implant_print_general.csv', 12)
+
+transpose_data('implant_print_start.csv', 'implant_print_start1.csv')
+implant_start_model = load_model('implant_print_start1.csv')
+uid_in_use.extend(get_uid(implant_start_model,2))
+print(len(uid_in_use))
+print(get_collisions(uid_in_use))
